5048e38b380f3581e1769f90ddc897e81f81e317
erustamovv09-netizen <e.rustamovv.09@gmail.com>
2026-05-08 16:22:05 -0700

    to'g'irlandi

--- mahsulot/models.py
+++ mahsulot/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone # Vaqt bilan ishlash uchun
 
 class Mahsulot(models.Model):
     # Mashina asosiy ma'lumotlari
@@ -12,13 +13,17 @@ class Mahsulot(models.Model):
     year = models.IntegerField()
     fuel_type = models.CharField(max_length=50)
     
-    # RASM: Mana shu qator bo'sh qolgan edi, uni qo'shdik
+    # RASM
     image = models.ImageField(upload_to='cars/', null=True, blank=True)
 
     # Kontakt ma'lumotlari
     owner_phone = models.CharField(max_length=20, default="+998901234567")
     telegram_user = models.CharField(max_length=100, default="Rustamovv_E")
     instagram_user = models.CharField(max_length=100, default="rustamovv.09")
+
+    # YANGI QO'SHILGAN QATORLAR (Tahrirlash va xavfsizlik uchun)
+    created_at = models.DateTimeField(default=timezone.now) # Qachon qo'shilgani
+    secret_key = models.CharField(max_length=100, blank=True, null=True) # Egasining maxfiy kaliti
 
     def __str__(self):
         return f"{self.name} - {self.owner_phone}"
